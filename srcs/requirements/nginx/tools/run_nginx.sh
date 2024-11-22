269d05428b17d1db5f6752cc147aa0b82543e889
Marie <mari.nazaryan7173@gmail.com>
2024-11-22 16:00:51 +0400

    run_nginx

---
+++ srcs/requirements/nginx/tools/run_nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout $KEYOUT -out $CERTS \
+	-subj "/C=AM/ST=Yerevan/L=Yerevan/O=42/OU=student/CN=$DOMAIN_NAME"
+
+cd /etc/nginx/conf.d/
+
+if [ -f /etc/nginx/conf.d/default.conf ]; then
+	sed -i "s#\$DOMAIN_NAME#$DOMAIN_NAME#g" /etc/nginx/conf.d/default.conf && \
+	sed -i "s#\$CERTS#$CERTS#g" /etc/nginx/conf.d/default.conf && \
+	sed -i "s#\$KEYOUT#$KEYOUT#g" /etc/nginx/conf.d/default.conf
+fi
+
+nginx -g "daemon off;"
