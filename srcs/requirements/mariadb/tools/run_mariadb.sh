c376c722887bcc0beae6a805db72a484549f1dbc
Marie <mari.nazaryan7173@gmail.com>
2024-12-11 18:41:36 +0400

    change mariadb owner

--- srcs/requirements/mariadb/tools/run_mariadb.sh
+++ srcs/requirements/mariadb/tools/run_mariadb.sh
@@ -4,6 +4,10 @@ service mariadb start
 
 sleep 2
 
+sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mariadb restart
+
 cat <<EOF > db.sql
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
@@ -16,5 +20,8 @@ mysql < db.sql
 rm db.sql
 
 service mariadb stop
+
+chown -R mysql:mysql /var/lib/mysql
+chmod 700 /var/lib/mysql/my_db
 
 exec mysqld
